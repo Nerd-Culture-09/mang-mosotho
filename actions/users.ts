b17267bd2ca0fa90981e1f510de384698e4f99cc
Fuma1322 <leonardfuma26@gmail.com>
2024-08-14 12:49:37 +0200

    debug 101

--- actions/users.ts
+++ actions/users.ts
@@ -5,58 +5,70 @@ import { BusinessRegisterInputProps, RegisterInputProps } from "@/types/types";
 import bcrypt from "bcrypt";
 import { NextApiRequest, NextApiResponse } from "next";
 
-
-// Function to create a new user
 export async function createUser(formdata: RegisterInputProps) {
-    const { fullName, email, phone,location, password, instaProfile, twitterProfile, facebookProfile,linkedProfile, role} = formdata;
+  const {
+    fullName,
+    email,
+    phone,
+    location,
+    password,
+    instaProfile,
+    twitterProfile,
+    facebookProfile,
+    linkedProfile,
+    role,
+  } = formdata;
 
-    try {
-        // Check if user with the same email exists
-        const existingUser = await prismaClient.user.findUnique({
-            where: {
-                email,
-            },
-        });
+  try {
+    // Check if user with the same email exists
+    const existingUser = await prismaClient.user.findUnique({
+      where: {
+        email,
+      },
+    });
 
-        if (existingUser) {
-            return {
-                data: null,
-                error: `User with this email (${email}) already exists in the Database`,
-                status: 409,
-            };
-        }
-
-        // Encrypt the Password using bcrypt
-        const hashedPassword = await bcrypt.hash(password, 10);
-       // Create the new user in the database
-        const newUser = await prismaClient.user.create({
-            data: {
-                name: fullName,
-                email,
-                phone,
-                location,
-                linkedProfile,
-                facebookProfile,
-                twitterProfile,
-                instaProfile,
-                password: hashedPassword,
-                role,
-            },
-        });
-        console.log(newUser);
-
-        return {
-            data: newUser,
-            error: null,
-            status: 200,
-        };
-    } catch (error) {
-        console.log(error);
-        return {
-            error: "Something went wrong",
-        };
+    if (existingUser) {
+      return {
+        data: null,
+        error: `User with this email (${email}) already exists in the Database`,
+        status: 409,
+      };
     }
+
+    // Encrypt the Password using bcrypt
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create the new user in the database
+    const newUser = await prismaClient.user.create({
+      data: {
+        name: fullName,
+        email,
+        phone,
+        location,
+        linkedProfile,
+        facebookProfile,
+        twitterProfile,
+        instaProfile,
+        password: hashedPassword,
+        role,
+      },
+    });
+
+    return {
+      data: newUser,
+      error: null,
+      status: 200,
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      data: null,
+      error: "Something went wrong",
+      status: 500,
+    };
+  }
 }
+
 
 // Function to fetch a user by ID
 export async function getUserById(id: string) {
